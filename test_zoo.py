8a239e10720902ffc2e42f184e998a9c7838a484
gaaf-noparuj <72190757+gaaf-noparuj@users.noreply.github.com>
2024-11-18 18:46:09 +0000

    add_testcase

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,23 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+
     # Add your additional test cases here.
+
+    def test_ticket1_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), None)
+        
+    def test_ticket2_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_ticket3_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+
+    def test_ticket4_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_ticket5_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
